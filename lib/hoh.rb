7619bf88485af05717632411f01fbc4d2206dc17
Kevin Clark <kevin.clark93@gmail.com>
2020-02-04 19:45:09 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,5 +43,26 @@ def naming_system
   # HoH all the way to Species!
   phylogeny = {
     label: "Kingdom"
+    sub_category: {
+      label: "Phylum"
+      sub_category: {
+        label: "Class"
+        sub_category: {
+          label: "Order"
+          sub_category: {
+            label: "Family"
+            sub_category: {
+              label: "Genus"
+              sub_category: {
+                label: "Species"
+                sub_category: nil
+              }
+            }
+          }
+        }
+        
+      }
+    }
   }
+  return phylogeny
 end
